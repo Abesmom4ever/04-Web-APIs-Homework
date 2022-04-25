381f00ea52a9ed5cc920ea13727dc20f9edb0fa0
Rejean Chung <rejeanpc@gmail.com>
2022-04-25 01:47:30 -0700

    Made basic HTML template and added initial vars and one event listener in JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".title").textContent = "Are you a Mean Girl?";
+document.querySelector(".comment").textContent =
+    "Take this quiz to see if you're a Mean Girl! Yiu have 30 seconds and, with every wrong answer, the time will decrease! Have fun and Good Luck!";
+
+var startButton = document.querySelector(".start-Button");
+
+startButton.addEventListener("click", function () {});
